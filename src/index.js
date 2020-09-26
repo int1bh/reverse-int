b14159cd1c5f5a4abd0b90c68a8a07179716285d
ALEXANDR <alexandrdryagin@yandex.ru>
2020-09-26 19:53:24 +0300

    complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
+    let nString = String(n).split('');
+    let filtered = nString.filter(element => {
+        if (element != '-') {
+            return element;
+        }
+        
+    } );
+    return Number((filtered.reverse().join('')));
+};
+
   
-}
